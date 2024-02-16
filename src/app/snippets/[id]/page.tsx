7cb6ddd4911dd1150512648ebb915c3d512e5a7d
kevin <wongkokwah@gmail.com>
2024-02-16 10:04:38 +0800

    generateStaticParams

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { db } from "@/db";
-import * as actions from '@/actions';
+import * as actions from "@/actions";
 
 interface SnippetShowPageProps {
   params: {
@@ -14,27 +14,38 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     where: { id: Number(props.params.id) },
   });
 
-  if(!snippet) {
+  if (!snippet) {
     return notFound();
   }
 
-  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id)
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
 
-  return <div>
-    <div className="flex m-4 justify-between items-center">
-      <h1 className="text-xl font-bold">
-        {snippet.title}
-      </h1>
-      <div>
-        <Link className="p-2 border rounded" href={`/snippets/${snippet.id}/edit`}>Edit</Link>
-        <form action={deleteSnippetAction}>
-          <button className="p-2 border rounded">Delete</button>
-
-        </form>
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div>
+          <Link className="p-2 border rounded" href={`/snippets/${snippet.id}/edit`}>
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
+        </div>
       </div>
+      <pre className="p-3 bg-gray-200 border rounded border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
     </div>
-    <pre className="p-3 bg-gray-200 border rounded border-gray-200" >
-      <code>{snippet.code}</code>
-    </pre>
-    </div>;
+  );
+}
+
+export async function generateStaticParams() {
+  const snippets = await db.snippets.findMany();
+
+  return snippets.map((snippet) => {
+    return {
+      id: snippet.id.toString()
+    }
+  });
 }
